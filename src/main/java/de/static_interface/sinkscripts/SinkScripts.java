8d0d311f25d3fe9955705174ecfdc51e486dd2f2
Trojaner <Trojaner25@gmail.com>
2014-05-19 22:10:38 +0300

    Initialize Commit

---
+++ src/main/java/de/static_interface/sinkscripts/SinkScripts.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2014 http://adventuria.eu, http://static-interface.de and contributors
+ *
+ *  This program is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 3 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.static_interface.sinkscripts;
+
+import de.static_interface.sinklibrary.SinkLibrary;
+import de.static_interface.sinklibrary.exceptions.NotInitializedException;
+import org.bukkit.Bukkit;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.logging.Level;
+
+public class SinkScripts extends JavaPlugin
+{
+    public void onEnable()
+    {
+        if ( !checkDependencies() ) return;
+        registerCommands();
+        registerListeners();
+    }
+
+    private boolean checkDependencies()
+    {
+        if ( Bukkit.getPluginManager().getPlugin("SinkLibrary") == null )
+        {
+            getLogger().log(Level.WARNING, "This Plugin requires SinkLibrary!");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return false;
+        }
+
+        if ( !SinkLibrary.initialized )
+        {
+            throw new NotInitializedException("SinkLibrary is not initialized!");
+        }
+        return true;
+    }
+
+    private void registerListeners()
+    {
+        Bukkit.getPluginManager().registerEvents(new ScriptChatListener(this), this);
+    }
+
+    private void registerCommands()
+    {
+        getCommand("script").setExecutor(new ScriptCommand(this));
+    }
+}
